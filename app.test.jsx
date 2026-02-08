19c955f75e928ff172a5df9b3b56ba9d9ce37f6b
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2026-02-08 16:01:53 +0100

    completed: simple ci-cd-project

---
+++ app.test.jsx
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import React from "react";
+import { render, screen } from "./utils/test-utils/test-utils.jsx";
+
+import App from "./src/App.jsx";
+
+describe("App", async () => {
+  it("should render while authenticating", () => {
+    render(<App />);
+
+    expect(screen.getByText("Vite + Reactooooo")).toBeInTheDocument();
+  });
+});
---
+++ utils/test-utils/test-utils.jsx
@@ -0,0 +1,3 @@
+import "@testing-library/jest-dom/vitest";
+// eslint-disable-next-line react-refresh/only-export-components
+export * from "@testing-library/react";
